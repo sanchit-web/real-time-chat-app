47d5a1343c05416ca2b5d8251387f93fec74e496
Sanchit Gupta <sanchit221206@gmail.com>
2026-07-15 01:37:26 +0000

    feat: add profile update with Cloudinary image upload

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import cloudinary from "../lib/cloudinary.js"
 
 // Signup a new user
 export const signup = async (req, res)=>{
@@ -58,4 +59,24 @@ export const checkAuth = (req, res)=>{
     res.json({success: true, user: req.user});
 }
 
-/
+// Controller to update user profile details
+export const updateProfile = async (req, res)=>{
+    try {
+        const { profilePic, bio, fullName } = req.body;
+
+        const userId = req.user._id;
+        let updatedUser;
+
+        if(!profilePic){
+            updatedUser = await User.findByIdAndUpdate(userId, {bio, fullName}, {new: true});
+        } else{
+            const upload = await cloudinary.uploader.upload(profilePic);
+
+            updatedUser = await User.findByIdAndUpdate(userId, {profilePic: upload.secure_url, bio, fullName}, {new: true});
+        }
+        res.json({success: true, user: updatedUser})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message})
+    }
+}
