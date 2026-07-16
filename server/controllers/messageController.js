da0148dd322c02fd8713230743014b0327ed28c3
Sanchit Gupta <sanchit221206@gmail.com>
2026-07-16 03:44:15 +0000

    feat: add chat message routes and sendMessage controller

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -59,3 +59,32 @@ export const markMessageAsSeen = async (req, res)=>{
         res.json({success: false, message: error.message})
     }
 }
+
+// Send message to selected user
+export const sendMessage = async (req, res) =>{
+    try {
+        const {text, image} = req.body;
+        const receiverId = req.params.id;
+        const senderId = req.user._id;
+
+        let imageUrl;
+        if(image){
+            const uploadResponse = await cloudinary.uploader.upload(image)
+            imageUrl = uploadResponse.secure_url;
+        }
+        const newMessage = await Message.create({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl
+        })
+
+        
+
+        res.json({success: true, newMessage});
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message})
+    }
+}
