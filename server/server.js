b18826ce2d1f434e9de781f222a942d05fb9ba8c
Sanchit Gupta <sanchit221206@gmail.com>
2026-07-16 03:21:18 +0000

    feat: add Message model and implement chat message controllers

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
+import userRouter from "./routes/userRoutes.js";
 
 // Create Express app and HTTP server
 const app = express();
@@ -14,6 +15,7 @@ app.use(cors());
 
 // Routes setup
 app.use("/api/status", (req, res)=> res.send("Server is live"));
+app.use("/api/auth", userRouter);
 
 // Connect to MongoDB
 await connectDB();
