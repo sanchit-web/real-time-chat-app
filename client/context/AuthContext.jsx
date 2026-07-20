2a1139bcca9e5061f1725b21afe82e4346fdc473
Sanchit Gupta <sanchit221206@gmail.com>
2026-07-20 04:21:18 +0000

    feat(auth): create AuthContext and global auth provider

---
+++ client/context/AuthContext.jsx
@@ -0,0 +1,29 @@
+import { createContext,useState } from "react";
+import axios from 'axios'
+
+
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
+axios.defaults.baseURL = backendUrl;
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children })=>{
+
+    const [token, setToken] = useState(localStorage.getItem("token"));
+    const [authUser, setAuthUser] = useState(null);
+    const [onlineUsers, setOnlineUsers] = useState([]);
+    const [socket, setSocket] = useState(null);
+
+    const value = {
+        axios,
+        authUser,
+        onlineUsers,
+        socket,
+    }
+
+    return (
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
