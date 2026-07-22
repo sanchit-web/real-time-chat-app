8ba1d21ed3a0b12eb4e4f6861cb7d9ff1749fa54
Sanchit Gupta <sanchit221206@gmail.com>
2026-07-22 14:01:20 +0000

    feat: add users fetching for chat sidebar

--- client/context/ChatContext.jsx
+++ client/context/ChatContext.jsx
@@ -1,11 +1,36 @@
-import { createContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { AuthContext } from "./AuthContext";
+
 
 export const ChatContext=createContext();
 
 export const ChatProvider=({children})=>{
 
+    const [messages, setMessages] = useState([]);
+    const [users, setUsers] = useState([]);
+    const [selectedUser, setSelectedUser] = useState(null)
+    const [unseenMessages, setUnseenMessages] = useState({})
+
+    const {socket, axios} = useContext(AuthContext);
+
+    // function to get all users for sidebar
+    const getUsers = async () =>{
+        try {
+            const { data } = await axios.get("/api/messages/users");
+            if (data.success) {
+                setUsers(data.users)
+                setUnseenMessages(data.unseenMessages)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+
+
     const value={
-        
+        messages, users, selectedUser, getUsers
+
     }
 
     return (
