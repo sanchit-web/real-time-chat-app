45feead1722b603cb591f83a940b647e86bacd02
Sanchit Gupta <sanchit221206@gmail.com>
2026-07-24 04:28:00 +0000

    Connect auth and chat context with sidebar, chat, and profile components

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,13 +1,26 @@
-import React, { useContext, useState } from 'react'
-import assets, { userDummyData } from '../assets/assets'
+import React, { useContext, useState ,useEffect} from 'react'
+import assets from '../assets/assets'
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthContext';
+import { ChatContext } from '../../context/ChatContext';
 
 
-const Sidebar = ({selectedUser,setSelectedUser}) => {
+const Sidebar = () => {
+
+    const {getUsers, users, selectedUser, setSelectedUser,
+        unseenMessages, setUnseenMessages } = useContext(ChatContext);
+
+    const {logout, onlineUsers} = useContext(AuthContext)
+
+    const [input, setInput] = useState(false)
+
     const navigate=useNavigate();
-    const {logout} = useContext(AuthContext)
+    
+    const filteredUsers = input ? users.filter((user)=>user.fullName.toLowerCase().includes(input.toLowerCase())) : users;
 
+     useEffect(()=>{
+        getUsers();
+    },[onlineUsers])
 
 
 return (
@@ -36,19 +49,19 @@ return (
 
 
     <div className='flex flex-col'>
-        {userDummyData.map((user, index)=>(
-            <div onClick={()=> {setSelectedUser(user)}}
+        {filteredUsers.map((user, index)=>(
+            <div onClick={()=> {setSelectedUser(user); setUnseenMessages(prev=> ({...prev, [user._id]:0}))}}
              key={index} className={`relative flex items-center gap-2 p-2 pl-4 rounded cursor-pointer max-sm:text-sm ${selectedUser?._id === user._id && 'bg-[#282142]/50'}`}>
                 <img src={user?.profilePic || assets.avatar_icon} alt="" className='w-[35px] aspect-[1/1] rounded-full'/>
                 <div className='flex flex-col leading-5'>
                     <p>{user.fullName}</p>
                     {
-                        index<3
+                        onlineUsers.includes(user._id)
                         ? <span className='text-green-400 text-xs'>Online</span>
                         : <span className='text-neutral-400 text-xs'>Offline</span>
                     }
                 </div>
-                {index > 2 && <p className='absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-violet-500/50'>{index}</p>}
+                {unseenMessages[user._id] > 0 && <p className='absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-violet-500/50'>{unseenMessages[user._id]}</p>}
             </div>
         ))}
     </div>
